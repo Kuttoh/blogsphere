d4d8cdce46aa6f02adfd6a0fbaff0ce8ec7b5f2c
Isaac Kuttoh <kuttohisaac@gmail.com>
2022-01-29 16:09:49 +0300

    init blogpost controller

---
+++ app/Http/Controllers/BlogPostController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CreateBlogPost;
+use App\Models\BlogPost;
+use App\Repositories\BlogPostRepository;
+use Illuminate\Http\Request;
+
+class BlogPostController extends Controller
+{
+    /**
+     * @var BlogPostRepository
+     */
+    private $postRepo;
+
+    public function __construct(BlogPostRepository $postRepository)
+    {
+        $this->postRepo = $postRepository;
+    }
+
+    public function index()
+    {
+        //fetch blog posts
+    }
+
+    public function create()
+    {
+        //create blog form
+    }
+
+    public function store(CreateBlogPost $request)
+    {
+        $validated = $request->except('_token');
+
+        $validated['publication_date'] = now();
+
+        $this->postRepo->store($validated);
+
+        return back()->with('success', 'Blog post successfully published!');
+    }
+
+    public function show(BlogPost $blogPost)
+    {
+        //show blogpost
+    }
+}
