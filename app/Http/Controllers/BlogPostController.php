80c613796a857b030e1e4df7a93566f02002c9fe
Isaac Kuttoh <kuttohisaac@gmail.com>
2022-01-31 09:29:43 +0300

    add minimal blog styling

--- app/Http/Controllers/BlogPostController.php
+++ app/Http/Controllers/BlogPostController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Http\Requests\CreateBlogPost;
 use App\Models\BlogPost;
 use App\Repositories\BlogPostRepository;
-use Illuminate\Http\Request;
 
 class BlogPostController extends Controller
 {
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('minimal');
 });
 
 Route::get('/dashboard', function () {
